f0f14ad71b2df0eeb0b20e235ed0dc6a15d78706
KristianKuznetsov <black.sky.dragon1@gmail.com>
2024-03-31 19:51:41 +0300

    myBranch commit 1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("Парам пам пам - мастер ветка комит 1 ");
         System.out.println("Пурум пум пум - мастер ветка комит 2 ");
         System.out.println("Пором пом пом - мастер ветка комит 3 ");
+
+
+        System.out.println("Я тучка-тучка-тучка,\n" + "Я вовсе не медведь.");
     }
 }
