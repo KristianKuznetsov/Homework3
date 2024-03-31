a0145b17008dfb0a5603590c8a2bd76a2a7cd05c
KristianKuznetsov <black.sky.dragon1@gmail.com>
2024-03-31 18:47:19 +0300

    master commit 3

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Парам пам пам - мастер ветка комит 1 ");
         System.out.println("Пурум пум пум - мастер ветка комит 2 ");
+        System.out.println("Пором пом пом - мастер ветка комит 3 ");
     }
 }
